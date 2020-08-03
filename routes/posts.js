f19fffcd5316f73e40461236ea6445138806c602
John Rice <john.rice.create@gmail.com>
2020-08-03 13:18:12 -0700

    Delete posts.js

--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const auth = require('../auth/auth')
-router.get('/posts', auth, (req, res) => {
-    res.send("You have made it to and authenticated route")
-
-})
-
-module.exports = router
